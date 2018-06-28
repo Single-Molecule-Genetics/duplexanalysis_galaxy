50e5f56e5e925166191990e8a8170aa3a7cb5bde
Monika Heinzl <k1556322@students.jku.at>
2018-06-28 11:35:25 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(name='duplexanalysis',
       version='0.0.1',
       description='Tool for analysing duplex sequencing data',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
-      packages=['funniest'],
+      url='https://github.com/monikaheinzl/duplexanalysis',
+      author='Monika Heinzl',
+      author_email='monika.heinzl@edumail.at',
+      packages=['duplexanalysis'],
       zip_safe=False)
