462ff4352d5902801fda2a4db001878a72f8daf1
Monika Heinzl <k1556322@students.jku.at>
2018-06-28 11:33:38 +0200

    changed name

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='duplexanalysis',
+      version='0.0.1',
+      description='Tool for analysing duplex sequencing data',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
